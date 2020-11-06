97817f5712aa3551b3c0011fe76c13e6d84e2c17
Purr Consuegra <purr.consuegra@solita.fi>
2020-11-06 10:49:01 +0200

    Add filling option to inputs

--- frontend/src/libraries/forms/Input.tsx
+++ frontend/src/libraries/forms/Input.tsx
@@ -4,12 +4,13 @@ import { asAccessibleField } from './FormField';
 import {Classes} from "@blueprintjs/core";
 
 interface BasicInputProps extends Omit<React.ComponentProps<"input">, "onChange"> {
-  onChange?: (value: string, event: React.ChangeEvent<HTMLInputElement>) => any
+  onChange?: (value: any, event: React.ChangeEvent<HTMLInputElement>) => any
+  fill?: boolean
 }
 
 export const BasicInput = React.forwardRef<HTMLInputElement, BasicInputProps>(
-  function BasicInput({className, onChange, ...props}, ref) {
-    return <input ref={ref} className={classNames(className, Classes.INPUT)}
+  function BasicInput({className, onChange, fill, ...props}, ref) {
+    return <input ref={ref} className={classNames(className, Classes.INPUT, fill && Classes.FILL)}
       onKeyDown={e => (e.key === 'Escape' || e.key === 'Enter') && (e.target as HTMLInputElement).blur()}
       onChange={onChange ? e => onChange(e.target.value, e) : undefined}
       {...props}
