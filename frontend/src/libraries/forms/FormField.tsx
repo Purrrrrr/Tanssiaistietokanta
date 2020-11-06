214e80e35f03b2cfa26469ff0118c808634212a4
Purr Consuegra <purr.consuegra@solita.fi>
2020-11-06 10:48:35 +0200

    Fix a bug in FormField

--- frontend/src/libraries/forms/FormField.tsx
+++ frontend/src/libraries/forms/FormField.tsx
@@ -17,11 +17,11 @@ export function asAccessibleField<T>(Component : React.JSXElementConstructor<T>)
 type Wrapper = (nodes: JSX.Element) => JSX.Element;
 
 export function useAccessibleField<T>(
-  {id, value, ...props} : FieldProps<T>
+  props : FieldProps<T>
 ) : {fieldProps: T, wrapField: Wrapper} {
-  const {fieldProps, addLabel} = useAccessibleLabeling({id, ...props});
-  const error = useError(value, props);
-  const errorId = `${id}-error`;
+  const {fieldProps, addLabel} = useAccessibleLabeling(props);
+  const error = useError(props.value, props);
+  const errorId = `${fieldProps.id}-error`;
   if (error) fieldProps['aria-describedby'] = errorId;
 
   const addValidation = (children : JSX.Element) => error ? <>{children}<ErrorMessage id={errorId} error={error} /></> : children;
