cad140e6eadbc40a1c0c6697e37e9b7a731a7695
Purr Consuegra <purr.consuegra@solita.fi>
2022-08-24 10:02:33 +0300

    Make parameters and connections available to services when data is requested through graphQL

--- frontend/src/backend/feathers.tsx
+++ frontend/src/backend/feathers.tsx
@@ -1,13 +1,18 @@
 import io from 'socket.io-client';
 import feathers from '@feathersjs/client';
 
-// Socket.io is exposed as the `io` global.
 const socket = io('http://localhost:8082/');
-
-// @feathersjs/client is exposed as the `feathers` global.
 const app = (feathers as unknown as Function)();
 
 app.configure(feathers.socketio(socket));
 app.configure(feathers.authentication());
 
 export default app
+// @ts-ignore
+window.feathers = app
+
+const graphQLService = app.service('graphql')
+
+export async function runGraphQlQuery({query, variables}) {
+  return await graphQLService.find({query: {query, variables}})
+}
