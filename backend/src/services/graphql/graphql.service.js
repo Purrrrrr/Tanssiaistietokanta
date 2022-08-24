97a5d19f28d2ede22f6bcc889ac67ea62d1399b4
Purr Consuegra <purr.consuegra@solita.fi>
2022-08-24 10:02:33 +0300

    Make parameters and connections available to services when data is requested through graphQL

--- backend/src/services/graphql/graphql.service.js
+++ backend/src/services/graphql/graphql.service.js
@@ -1,27 +1,31 @@
 // Initializes the `graphql` service on path `/graphql`
 const { ApolloServer } = require('apollo-server-express');
-const { graphql } = require('graphql');
 const schema = require('./graphql.schema');
 const getResolvers = require('./graphql.resolvers');
 const createService = require('./graphql.feathers-service');
-const logger = require('../../logger');
 const hooks = require('./graphql.hooks');
-const {buildSchema} = require('graphql');
 
 module.exports = function (app) {
   const resolvers = getResolvers(app);
-  const context = () => ({ app });
+  const context = ({
+    req = {headers: {}},
+    context = {}
+  }) => ({
+    headers: req.headers,
+    ...context,
+    provider: 'graphql',
+  });
 
   const server = new ApolloServer({ typeDefs: schema, resolvers, context});
   server.applyMiddleware({ app });
 
   // Initialize our service with any options it requires
-  app.use('/graphql', createService(schema, resolvers, app));
+  app.use('/graphql', createService(schema, resolvers, server));
   app.use('/graphql-schema', {
     async find() {
       return schema;
     }
-  }, (req, res, next) => {
+  }, (req, res) => {
     const {data} = res;
     res.format({
       'text/plain': () => res.send(data),
