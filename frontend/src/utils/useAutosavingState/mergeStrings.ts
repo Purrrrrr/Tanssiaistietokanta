7470b4c559490b9d776f896011aa7401fdaff169
Purr Consuegra <Ext-purr.consuegra@tulli.fi>
2022-09-14 06:42:12 +0300

    Join strings more cleverly

--- frontend/src/utils/useAutosavingState/mergeStrings.ts
+++ frontend/src/utils/useAutosavingState/mergeStrings.ts
@@ -1,15 +1,24 @@
 import {merge as merge3} from 'node-diff3'
 import {MergeData, MergeResult} from './types'
 
+const wordBoundary = (() => {
+  try {
+    return /(?<=\p{White_Space}+)(?!\p{White_Space}+)|(?<!\p{White_Space}+)(?=\p{White_Space}+)/u
+    // eslint-disable-next-line no-unreachable
+  } catch(e) {
+    return /\b/
+  }
+})()
+
 export function mergeConflictingStrings(
   {server, original, local, key} : MergeData<string>
 ) : MergeResult<string> {
-  const { conflict, result } = merge3(server, original, local, {stringSeparator: "\n"})
+  const { conflict, result } = merge3(server, original, local, {stringSeparator: wordBoundary})
   
   if (!conflict) {
     return {
       state: 'MODIFIED_LOCALLY',
-      pendingModifications: result.join("\n"),
+      pendingModifications: result.join(),
       conflicts: [],
     }
   }
