23f8a4d5c06531d419821b5103d92437ec0b7b68
Purr Consuegra <Ext-purr.consuegra@tulli.fi>
2022-10-05 07:51:44 +0300

    Fancy autohiding sync-status

--- frontend/src/components/SyncStatus.tsx
+++ frontend/src/components/SyncStatus.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import {Icon} from "libraries/ui";
+import React, {useRef, useState, useEffect} from 'react';
+import classNames from 'classnames';
+import {Icon, IconName, Intent} from "libraries/ui";
 import {SyncState} from 'utils/useAutosavingState';
 
-export default function SyncStatus({state} : { state : SyncState }) {
-  switch(state) {
-    case 'IN_SYNC':
-      return <><Icon icon="saved" intent="success" /> Tallennettu</>
-    case 'MODIFIED_LOCALLY':
-      return <><Icon icon="refresh" intent="primary" /> Tallennetaan...</>
-    case 'CONFLICT':
-      return <><Icon icon="outdated" intent="warning" /> Synkronointivirhe</>
-  }
-  return null
+import './SyncStatus.sass'
+
+const icons : Record<SyncState, IconName> = {
+  IN_SYNC: 'saved',
+  MODIFIED_LOCALLY: 'refresh',
+  CONFLICT: 'outdated',
+}
+const iconIntents : Record<SyncState, Intent> = {
+  IN_SYNC: 'success',
+  MODIFIED_LOCALLY: 'primary',
+  CONFLICT: 'warning',
+}
+const texts : Record<SyncState, string> = {
+  IN_SYNC: 'Tallennettu',
+  MODIFIED_LOCALLY: 'Tallennetaan...',
+  CONFLICT: 'Synkronointivirhe',
+}
+
+export default function SyncStatus({state, block, className, style} : { state : SyncState, block?: boolean, className?: string, style?: React.CSSProperties }) {
+  const previousState = useRef<SyncState | null>(null)
+  const [changed, setChanged] = useState(false)
+  useEffect(() => {
+    if (previousState.current !== state && previousState.current !== null) {
+      setChanged(true)
+    }
+    previousState.current = state
+
+    const id = setTimeout(() => {
+      setChanged(false)
+    }, 1000)
+    return () => clearTimeout(id)
+  }, [state])
+
+  return <span
+    style={style}
+    className={classNames(className, 'sync_status', state.toLowerCase(), {'status-changed': changed, block})}
+  >
+    <Icon icon={icons[state]} intent={iconIntents[state]} />
+    <span className="text">{texts[state]}</span>
+  </span>
 }
 
