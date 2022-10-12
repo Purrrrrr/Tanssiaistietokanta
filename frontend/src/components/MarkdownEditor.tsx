bdc6d9a9889e72b5377118a95698ff16bfe0c807
Purr Consuegra <Ext-purr.consuegra@tulli.fi>
2022-10-12 10:11:00 +0300

    Analyze build size, switch to a lighter markdown editor component

--- frontend/src/components/MarkdownEditor.tsx
+++ frontend/src/components/MarkdownEditor.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {FormGroup} from "libraries/ui";
-import MDEditor from '@uiw/react-md-editor';
-import rehypeSanitize from "rehype-sanitize";
+import MdEditor from 'react-markdown-editor-lite';
+import Markdown from 'markdown-to-jsx';
 
+import 'react-markdown-editor-lite/lib/index.css';
 import './MarkdownEditor.sass'
 
 type MDEditorProps = {
@@ -10,7 +11,7 @@ type MDEditorProps = {
   labelStyle?: 'inline'
   value: any,
   height?: number,
-  onChange: (v: any) => any,
+  onChange: (v: string) => any,
 }
 
 export function MarkdownEditor({label, labelStyle, ...props} : MDEditorProps) {
@@ -19,17 +20,11 @@ export function MarkdownEditor({label, labelStyle, ...props} : MDEditorProps) {
   </FormGroup>
 }
 
-export function SimpleMarkdownEditor({value, ...props}) {
-  return <MDEditor
-    commandsFilter={cmd =>
-      //TODO: make the buttons accessible with the keyboard WITHOUT making navigation horrible
-      ({...cmd, buttonProps: {...cmd.buttonProps, tabIndex: -1}})
-    }
-    defaultTabEnable={true}
-    previewOptions={{
-      rehypePlugins: [[rehypeSanitize]],
-    }}
+export function SimpleMarkdownEditor({value, onChange, ...props}) {
+  return <MdEditor
+    renderHTML={(text : string) => <Markdown>{text}</Markdown>}
     value={value ?? ""}
+    onChange={({text}) => onChange(text)}
     {...props}
   />
 }
