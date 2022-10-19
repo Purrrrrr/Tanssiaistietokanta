ec3607376ad5a5fc626957c2b9c8c6f53642db8d
Purr Consuegra <purr.consuegra@solita.fi>
2022-10-19 08:31:55 +0300

    Debug logging for GraphQL queries

--- frontend/src/backend/feathers.tsx
+++ frontend/src/backend/feathers.tsx
@@ -1,6 +1,10 @@
 import {FetchResult} from '@apollo/client'
+import {print} from 'graphql'
 import io from 'socket.io-client'
 import feathers from '@feathersjs/client'
+import createDebug from 'utils/debug'
+
+const debug = createDebug('graphql')
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -18,5 +22,14 @@ export default app
 const graphQLService = app.service('graphql')
 
 export async function runGraphQlQuery({query, variables}) : Promise<FetchResult<any, any, any>> {
-  return await graphQLService.find({query: {query, variables}})
+  debug('GraphQL query: %s\nVariables: %O', print(query), variables)
+  const result = await graphQLService.find({query: {query, variables}})
+  if (debug.enabled) {
+    Object.keys(result).forEach(
+      key => Object.keys(result[key]).forEach(
+        dataKey => debug('%s.%s: %O', key, dataKey, result[key][dataKey])
+      )
+    )
+  }
+  return result
 }
