0100560ce48884d4f34ccdbba49c7216bfc64cfb
Purr Consuegra <purr.consuegra@solita.fi>
2022-10-24 10:13:39 +0300

    Better cache policy

--- frontend/src/backend/index.tsx
+++ frontend/src/backend/index.tsx
@@ -33,7 +33,7 @@ export function entityListQueryHook<T extends {[k in number]: Entity[]}, V>(
     //TODO: type this
     useServiceListEvents(service, callbacks)
 
-    const result = useQuery<T, V>(compiledQuery, { fetchPolicy: 'network-only' })
+    const result = useQuery<T, V>(compiledQuery, { fetchPolicy: 'cache-and-network' })
     const empty = [] as ValueOf<T>
     const data = result.data !== undefined
       ? getSingleValue(result.data)
@@ -132,6 +132,6 @@ export function backendQueryHook<T, V>(
   query: TypedDocumentNode<T, V>
 ): ((v: V, o?: QueryHookOptions<T, V>) => QueryResult<T, V>) {
   return (variables: V, options: QueryHookOptions<T, V> = {}) => {
-    return useQuery<T, V>(query, { variables, fetchPolicy: 'network-only', ...options })
+    return useQuery<T, V>(query, { variables, fetchPolicy: 'cache-and-network', ...options })
   }
 }
