d83177e0f5855f9d1ee203c05f9083bfcb77dc6f
Purr Consuegra <purr.consuegra@solita.fi>
2022-11-25 13:36:54 +0200

    Remove unused console.log code

--- frontend/src/libraries/ui/AutosizedSection.tsx
+++ frontend/src/libraries/ui/AutosizedSection.tsx
@@ -16,11 +16,11 @@ export function AutosizedSection({children, className, ...props}) {
 
     const paddingX = parseFloat(containerStyle.paddingLeft) + parseFloat(containerStyle.paddingRight)
     const paddingY = parseFloat(containerStyle.paddingTop) + parseFloat(containerStyle.paddingBottom)
-    console.log(paddingX, paddingY)
+    // console.log(paddingX, paddingY)
     const width = innerSize.inlineSize / (container.current.offsetWidth - paddingX)
     const height = innerSize.blockSize / (container.current.offsetHeight - paddingY)
     const overFlowAmount = Math.max(1, width, height)
-    console.log(width, height, overFlowAmount)
+    // console.log(width, height, overFlowAmount)
 
     setSize(1 / overFlowAmount)
   }
