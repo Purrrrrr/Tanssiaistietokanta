4307148614a44bf9154c02f420383b3b044424bf
Purr Consuegra <purr.consuegra@solita.fi>
2023-03-13 11:00:16 +0200

    Versioning based on saving a new version every time after editing has paused for a while

--- backend/src/utils/versioningNedbService.js
+++ backend/src/utils/versioningNedbService.js
@@ -1,12 +1,11 @@
 const createNedbService = require('feathers-nedb')
 const NeDB = require('@seald-io/nedb')
-const {omit} = require('lodash')
+const {debounce} = require('lodash')
 
-/* Two databases:
- * Latest:
- *
- * in memory
- * data straight with _version and _basedOn
+const VERSION_SAVE_DELAY_MS = 5000
+const MAX_VERSION_SAVE_DELAY_MS = 60000
+
+/* Two databases: Latest and versions
  *
  * Versions:
  *
@@ -14,12 +13,13 @@ const {omit} = require('lodash')
  *   _id //version id
  *   _recordId //Common id for different versions
  *   _version //version number
- *   _basedOn //previous version, used for undo
  * }
  */
 module.exports = function(options) {
   const currentService = createNedbService(options)
   const versionService = getVersionService(options)
+
+  const versionStoreFunctions = new Map()
 
   return {
     getModel: () => currentService.getModel(),
@@ -27,57 +27,40 @@ module.exports = function(options) {
     find: (params) => currentService.find(params),
     get: (id, params) => currentService.get(id, params),
     create: async (data, params) => {
-      const res = await currentService.create(
-        map(data, addFirstVersionInfo),
-        params,
-      )
+      const res = await currentService.create(addTimestamps(data, now()), params)
 
-      return createVersionData(res, params)
+      return queueVersionSave(res)
     },
-    update: async (id, data, {query, ...params}) => {
-      const res = await updateAndVersionRecords(id, query, (_id, versionData)=> {
-        const patch = { ...data, ...versionData }
-        return currentService.update(_id, patch, params)
-      })
+    update: async (id, data, params) => {
+      const res = await currentService.update(id, addTimestamps(data), params)
 
-      return createVersionData(res, params)
+      return queueVersionSave(res)
     },
-    patch: async (id, data, {query, ...params}) => {
-      const cleanedData = cleanUpdate(data)
+    patch: async (id, data, params) => {
+      const res = await currentService.patch(id, addTimestamps(data), params)
 
-      const res = await updateAndVersionRecords(id, query, (_id, versionData)=> {
-        const patch = { ...cleanedData, ...versionData }
-        return currentService.patch(_id, patch, params)
-      })
-
-      return createVersionData(res, params)
+      return queueVersionSave(res, params)
     },
     remove: (id, params) => {
       return currentService.remove(id, params)
     },
   }
 
-  async function updateAndVersionRecords(id, query, func) {
-    const existingData = await currentService._findOrGet(id, {query})
-
-    return runForAll(existingData, record =>
-      func(record._id, {
-        _basedOn: record._version,
-        _version: (record._version ?? 0) + 1,
-      })
-    )
-  }
-
-  async function createVersionData(res, params) {
-    await runForAll(res, ({_id, ...record}) =>
-      versionService.create({...record, _recordId: _id}, params)
+  async function queueVersionSave(res) {
+    runForAll(res, ({_id, ...record}) =>
+      getDebouncedSaveVersion(_id)({...record, _recordId: _id})
     )
     return res
   }
-}
 
-function addFirstVersionInfo(record) {
-  return {...record, _basedOn: null, _version: 1}
+  function getDebouncedSaveVersion(id) {
+    return computeIfAbsent(versionStoreFunctions, id, () =>
+      debounce((data) => {
+        versionStoreFunctions.delete(id)
+        versionService.create(data)
+      }, VERSION_SAVE_DELAY_MS, {maxWait: MAX_VERSION_SAVE_DELAY_MS})
+    )
+  }
 }
 
 function getVersionService(options) {
@@ -86,8 +69,9 @@ function getVersionService(options) {
   const versionDb = new NeDB({
     filename: versionDBFilename,
     autoload: true,
+
   })
-  versionDb.ensureIndex({fieldName: ['_recordId', '_version']})
+  versionDb.ensureIndex({fieldName: ['_recordId', '_id']})
   return createNedbService({Model: versionDb})
 }
 
@@ -99,20 +83,24 @@ function map(data, func) {
 
 function runForAll(data, func) {
   return Array.isArray(data)
-    ? Promise.all(data.map(func))
+    ? data.forEach(func)
     : func(data)
 }
 
-function cleanUpdate(update) {
-  const patch = {
-    $set: {},
+function computeIfAbsent(map, key, getDefault) {
+  if (map.has(key)) {
+    return map.get(key)
   }
-  for(const key of Object.keys(update)) {
-    if (key[0] === '$') {
-      patch[key] = omit(update[key], '_version')
-    } else {
-      patch.$set[key] = update[key]
-    }
-  }
-  return patch
+  const def = getDefault()
+  map.set(key, def)
+  return def
 }
+
+function addTimestamps(data, _createdAt) {
+  return map(data, item => ({
+    ...item,
+    ...(_createdAt ? {_createdAt} : {}),
+    _updatedAt: now()
+  }))
+}
+const now = () => new Date().toISOString()
