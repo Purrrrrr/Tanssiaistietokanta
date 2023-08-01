187b1b1846449e36a46c23ee9d1776950aee457b
Purr Consuegra <purr.consuegra@solita.fi>
2023-08-01 22:27:07 +0300

    Fix LinkMenuItem href

--- frontend/src/components/widgets/LinkMenuItem.tsx
+++ frontend/src/components/widgets/LinkMenuItem.tsx
@@ -3,8 +3,8 @@ import {useLinkClickHandler} from 'react-router-dom'
 
 import {MenuItem, MenuItemProps} from 'libraries/ui'
 
-export function LinkMenuItem({ href, ...props}: MenuItemProps & {href: string}) {
-  const onClick =  useLinkClickHandler(href)
+export function LinkMenuItem(props: MenuItemProps & {href: string}) {
+  const onClick =  useLinkClickHandler(props.href)
 
   return <MenuItem onClick={onClick as MenuItemProps['onClick']} {...props} />
 }
