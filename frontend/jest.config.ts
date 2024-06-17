9ce945c6f69773536a47512bb922fa1b9a643d86
Purr Consuegra <purr.consuegra@solita.fi>
2024-06-17 07:42:24 +0300

    Use babel-jest for faster compilation times

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,7 +2,6 @@ import type {Config} from 'jest'
 
 const config: Config = {
   verbose: true,
-  preset: 'ts-jest/presets/js-with-ts',
   testPathIgnorePatterns: [
     '<rootDir>/cypress/',
     '<rootDir>/node_modules/',
