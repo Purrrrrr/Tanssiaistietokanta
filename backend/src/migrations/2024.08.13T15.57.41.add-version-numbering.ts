660e2be89c35890e17262fcbcbae3f60fa0802ec
Purr Consuegra <purr.consuegra@solita.fi>
2024-08-14 10:56:49 +0300

    Version numbering migration

---
+++ backend/src/migrations/2024.08.13T15.57.41.add-version-numbering.ts
@@ -0,0 +1,34 @@
+import { MigrationFn } from '../umzug.context';
+
+export const up: MigrationFn = async params => {
+  const { context } = params
+  const serviceNames = ['events', 'workshops', 'dances'] as const
+  for (const name of serviceNames) {
+    const model = context.getModel(name)
+    const versionModel = context.getVersionModel(name)
+    const counter = new VersionCounter()
+
+    const records = await versionModel.findAsync({}).sort({ _updatedAt: 1 })
+    for (const record of records) {
+      const { _id, _recordId } = record
+      const versionNumber = counter.getVersionFor(_recordId)
+      await versionModel.updateAsync({ _id }, { $set: { versionNumber } })
+    }
+    for (const [_id, versionNumber] of counter.versionNumberMap.entries()) {
+      await model.updateAsync({ _id }, { $set: { versionNumber } })
+    }
+  }
+
+}
+export const down: MigrationFn = async () => {};
+
+class VersionCounter {
+  versionNumberMap = new Map<string, number>()
+
+  getVersionFor(id: string): number {
+    const version = (this.versionNumberMap.get(id) ?? -1) + 1
+
+    this.versionNumberMap.set(id, version)
+    return version
+  }
+}
