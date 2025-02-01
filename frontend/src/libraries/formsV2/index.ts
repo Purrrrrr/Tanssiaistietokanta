813e4fda73854578621a8b1c3d5c0d61c4333b3b
Purr Consuegra <purr.consuegra@solita.fi>
2025-02-01 22:41:07 +0200

    Reorganize

--- frontend/src/libraries/formsV2/index.ts
+++ frontend/src/libraries/formsV2/index.ts
@@ -27,3 +27,44 @@ TypedStringPath
 useAutosavingState
 UseAutosavingStateReturn
 */
+
+import { type FieldInputComponent, TextInput } from './components/inputs'
+import { type FieldProps, Field, withComponent } from './Field'
+import type { FieldPath } from './types'
+
+export { TextInput }
+
+/* TODO:
+ *
+ * working wrapper
+ * validation
+ * conflict handling
+ * input field components
+ * list editor
+ * Translator into form -> auto translated labels
+ *
+ * DONE:
+ * reducer logic, types etc.
+ *
+ */
+
+const TextField = withComponent(TextInput)
+
+type ExternalFieldProps<Data, Input, Output extends Input, Extra extends object> = FieldProps<Input, Output, Extra> & {
+  path: FieldPath<Input, Output, Data>
+}
+type SpecializedFieldComponentFor<Data, Input, Output extends Input, Extra extends object> = (p: Omit<ExternalFieldProps<Data, Input, Output, Extra>, 'component'>) => React.ReactElement
+
+interface FormFor<Data> {
+  Field: <Input, Output extends Input, Extra extends object>(props: ExternalFieldProps<Data, Input, Output, Extra>) => React.ReactElement
+  withComponent: <Input, Output extends Input, Extra extends object>(c: FieldInputComponent<Input, Output, Extra>) => SpecializedFieldComponentFor<Data, Input, Output, Extra>
+  TextField: SpecializedFieldComponentFor<Data, string | undefined | null, string, object>
+}
+
+export function formFor<Data>(): FormFor<Data> {
+  return {
+    Field,
+    withComponent,
+    TextField: TextField
+  } as FormFor<Data>
+}
