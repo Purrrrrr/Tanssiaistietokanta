67f6114e4cfca7722e7df984e9a3773dc226f55a
Purr Consuegra <purr.consuegra@solita.fi>
2025-02-11 22:12:12 +0200

    Fix input in style of form v1

--- frontend/src/libraries/formsV2/components/inputs/TextInput.tsx
+++ frontend/src/libraries/formsV2/components/inputs/TextInput.tsx
@@ -1,4 +1,14 @@
+import {ComponentProps} from 'react'
+import {Classes} from '@blueprintjs/core'
+import classNames from 'classnames'
+
 import type { FieldInputComponent } from './types'
 
-export const TextInput : FieldInputComponent<string | undefined | null, string> = ({value, onChange, ...rest}) =>
-  <input value={value ?? ''} onChange={e => onChange(e.target.value)} {...rest} />
+export const TextInput : FieldInputComponent<string | undefined | null, string, ComponentProps<'input'>> = ({value, onChange, className, ...rest}) =>
+  <input
+    value={value ?? ''}
+    className={classNames(className, Classes.INPUT)}
+    onKeyDown={e => (e.key === 'Escape' || e.key === 'Enter') && (e.target as HTMLInputElement).blur()}
+    onChange={e => onChange(e.target.value)}
+    {...rest}
+  />
