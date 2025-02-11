a6e24d010798e1f1d9a60a48d7c4581664cecd9a
Purr Consuegra <purr.consuegra@solita.fi>
2025-02-11 20:46:06 +0200

    BareFieldContainer

--- frontend/src/libraries/formsV2/Field.tsx
+++ frontend/src/libraries/formsV2/Field.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useId, useState, useSyncExternalStore } from 'react'
 
-import { type ExternalFieldContainerProps, FieldContainer } from './components/FieldContainer'
+import { type ExternalBareFieldContainerProps, type ExternalFieldContainerProps, BareFieldContainer, FieldContainer } from './components/FieldContainer'
 import type { FieldInputComponent, OmitInputProps } from './components/inputs'
 import { useFormContext } from './context'
 import { change, setValidationResult } from './reducer'
@@ -9,9 +9,20 @@ import { validate } from './utils/validation'
 
 /* TODO:
  *
- * input field components
+ * FieldSet component
+ *   wrapper?: none | fieldset
  * working wrapper
+ *  always use <label>
+ *  error-reference
+ *  input id?
+ *  read only mode
+ *  inline
  * list editor
+ *   no wrapper
+ * fieldset wrapper?
+ * a11y-announcement element for list editor and such
+ * input field components
+ * translations
  * conflict handling
  *
  * DONE:
@@ -25,18 +36,42 @@ export type FieldProps<Input, Output extends Input, Extra extends object>  = {
   component: FieldInputComponent<Input, Output, Extra>
 } & OmitInputProps<Extra> & ExternalFieldContainerProps & ValidationProps
 
-export function Field<Input, Output extends Input, Extra extends object>({path, label, component: C, required, schema, ...extra}: FieldProps<Input, Output, Extra>) {
+export function Field<Input, Output extends Input, Extra extends object>({path, containerClassName, label, labelStyle, labelInfo, helperText, component: C, required, schema, ...extra}: FieldProps<Input, Output, Extra>) {
   const id = useId()
-  const errorId = `${id}-error`
-  const { inputProps, error } = useFieldValueProps<Input, Output>(path, { required, schema })
+  const { inputProps, containerProps } = useFieldValueProps<Input, Output>(path, { required, schema })
 
-  return <FieldContainer label={label} id="" error={error} errorId={errorId} labelStyle="above">
+  return <FieldContainer
+    label={label}
+    labelFor={id}
+    labelStyle={labelStyle}
+    labelInfo={labelInfo}
+    helperText={helperText}
+    containerClassName={containerClassName}
+    {...containerProps}
+  >
     <C id={id} {...inputProps} {...extra as Extra} />
   </FieldContainer>
 }
 
+
+export type UnwrappedFieldProps<Input, Output extends Input, Extra extends object>  = {
+  path: PathFor<Input>
+  component: FieldInputComponent<Input, Output, Extra>
+} & OmitInputProps<Extra> & ExternalBareFieldContainerProps & ValidationProps
+
+export function UnwrappedField<Input, Output extends Input, Extra extends object>({path, label, component: C, required, schema, ...extra}: UnwrappedFieldProps<Input, Output, Extra>) {
+  const id = useId()
+  const { inputProps, containerProps } = useFieldValueProps<Input, Output>(path, { required, schema })
+
+  return <BareFieldContainer {...containerProps} label={label} labelFor={id}>
+    <C id={id} {...inputProps} {...extra as Extra} />
+  </BareFieldContainer>
+}
+
+
 function useFieldValueProps<Input, Output extends Input, Data = unknown>(path: PathFor<Data>, validation: ValidationProps) {
   const id = `${path}:${useId()}`
+  const errorId = `${id}-error`
   const { getState, getValueAt, dispatch, subscribe, subscribeTo } = useFormContext()
   const [value, setValue] = useState(() => getValueAt<Input>(path))
   const error = useSyncExternalStore(subscribeTo(path), () => getState().errors[id])
@@ -61,6 +96,9 @@ function useFieldValueProps<Input, Output extends Input, Data = unknown>(path: P
   const inputProps = {
     value, onChange,
   }
+  const containerProps = {
+    error, errorId,
+  }
 
-  return { value, onChange, error, inputProps }
+  return { inputProps, containerProps }
 }
