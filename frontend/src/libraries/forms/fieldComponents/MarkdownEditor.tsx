8485da5d9475185cb810f022113b17a0340e91f3
Purr Consuegra <purr.consuegra@solita.fi>
2025-02-20 10:58:11 +0200

    Use function components as MarkdownEditor toolbar plugins

--- frontend/src/libraries/forms/fieldComponents/MarkdownEditor.tsx
+++ frontend/src/libraries/forms/fieldComponents/MarkdownEditor.tsx
@@ -1,70 +1,55 @@
 import 'react-markdown-editor-lite/lib/index.css'
 import React from 'react'
-import MdEditor, { PluginComponent, Plugins }  from 'react-markdown-editor-lite'
+import MdEditor, { Plugins }  from 'react-markdown-editor-lite'
 import { MarkdownToJSX } from 'markdown-to-jsx'
 
 import {AnchorButton, Markdown} from 'libraries/ui'
 
-import { FormMetadataContext, useFormStrings } from '../formContext'
-import { FormStrings } from '../strings'
+import { useFormStrings } from '../formContext'
 import {FieldComponentProps} from '../types'
 
 const defaultQRCode = '<QR title="..." value="https//..." size={250} />'
-class QRCode extends PluginComponent<object> {
-  // Define plugin name here, must be unique
-  static pluginName = 'qrcode'
-  // Define which place to be render, default is left, you can also use 'right'
-  static align = 'left'
-  static contextType = FormMetadataContext
-
-  render() {
-    const texts = pluginToStrings(this) ?? {
-      insertQRCode: '',
-    }
-    return (
-      <span
-        className="button"
-        title={texts.insertQRCode}
-        onClick={() => this.editor.insertText(defaultQRCode)}
-      >
-        QR
-      </span>
-    )
-  }
+function QRCode({ editor }) {
+  const { insertQRCode } = useFormStrings().markdownEditor
+  return (
+    <span
+      className="button"
+      title={insertQRCode}
+      onClick={() => editor.insertText(defaultQRCode)}
+    >
+      QR
+    </span>
+  )
 }
+QRCode.align = 'left'
+QRCode.pluginName = 'qrcode'
 
-class HelpLink extends PluginComponent<object> {
-  // Define plugin name here, must be unique
-  static pluginName = 'helplink'
-  // Define which place to be render, default is left, you can also use 'right'
-  static align = 'right'
-  static contextType = FormMetadataContext
-
-  render() {
-    const texts = pluginToStrings(this) ?? {
-      helpUrl: '',
-      help: '',
-    }
-    return (
-      <AnchorButton
-        intent="primary"
-        small
-        target="_blank"
-        href={texts.helpUrl}
-      >
-        {texts.help}
-      </AnchorButton>
-    )
-  }
+function HelpLink() {
+  const { helpUrl, help } = useFormStrings().markdownEditor
+  return (
+    <AnchorButton
+      intent="primary"
+      small
+      target="_blank"
+      href={helpUrl}
+    >
+      {help}
+    </AnchorButton>
+  )
 }
+HelpLink.align = 'right'
+HelpLink.pluginName = 'helplink'
 
-function pluginToStrings(plugin): FormStrings['markdownEditor'] | undefined {
-  return plugin.context?.getStrings?.()?.markdownEditor
-}
-
-MdEditor.use(QRCode, {})
-MdEditor.use(HelpLink, {})
-MdEditor.unuse(Plugins.Image)
+const pluginList = [
+  Plugins.Header, Plugins.FontBold, Plugins.FontItalic, Plugins.FontUnderline,
+  Plugins.FontStrikethrough, Plugins.ListUnordered, Plugins.ListOrdered,
+  Plugins.BlockWrap, Plugins.BlockCodeInline, Plugins.BlockCodeBlock, Plugins.Table,
+  Plugins.Link, Plugins.Logger, Plugins.ModeToggle, Plugins.FullScreen,
+  QRCode,
+  HelpLink,
+]
+MdEditor.unuseAll()
+pluginList.forEach(plugin => MdEditor.use(plugin, {}))
 
 interface MarkdownEditorProps extends FieldComponentProps<string, HTMLTextAreaElement> {
   style?: React.CSSProperties
