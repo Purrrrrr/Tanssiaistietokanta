c0d668c8016b366914ac37de5855b2d181311b3e
Purr Consuegra <purr.consuegra@solita.fi>
2025-03-01 11:17:09 +0200

    Fix TextInput fill class

--- frontend/src/libraries/formsV2/components/inputs/TextInput.tsx
+++ frontend/src/libraries/formsV2/components/inputs/TextInput.tsx
@@ -4,10 +4,10 @@ import classNames from 'classnames'
 
 import type { FieldInputComponent } from './types'
 
-export const TextInput : FieldInputComponent<string, ComponentProps<'input'>> = ({value, onChange, className, ...rest}) =>
+export const TextInput : FieldInputComponent<string, ComponentProps<'input'>> = ({value, onChange, className, inline, ...rest}) =>
   <input
     value={value ?? ''}
-    className={classNames(className, Classes.INPUT)}
+    className={classNames(className, Classes.INPUT, inline || Classes.FILL)}
     onKeyDown={e => (e.key === 'Escape' || e.key === 'Enter') && (e.target as HTMLInputElement).blur()}
     onChange={e => onChange(e.target.value)}
     {...rest}
