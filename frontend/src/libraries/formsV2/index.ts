c521481f7d81284ff1c9cf7c7cd9c674b1e22873
Purr Consuegra <purr.consuegra@solita.fi>
2025-03-06 08:06:49 +0200

    More clearer types for asFormField

--- frontend/src/libraries/formsV2/index.ts
+++ frontend/src/libraries/formsV2/index.ts
@@ -1,13 +1,13 @@
 import { type ComponentProps, type ComponentType } from 'react'
 
-import type { FieldPath, Labelable, ValueAt } from './types'
+import type { Labelable, ValueAt } from './types'
 
-import { type FieldInputComponent, type FieldInputComponentProps, MarkdownInput, Nullable, SwitchInput, TextInput } from './components/inputs'
+import { type FieldInputComponent, type FieldInputComponentProps, type Nullable, MarkdownInput, SwitchInput, TextInput } from './components/inputs'
 import { SwitchProps } from './components/inputs/SwitchInput'
 import { type FieldProps, type SelfLabeledFieldProps, Field } from './Field'
 import { type FormProps, Form } from './Form'
 import { useApplyAt, useChangeAt, useValueAt } from './hooks'
-import { asFormField, asSelfLabeledFormField } from './utils/asFormField'
+import { type SpecializedFieldComponent, asFormField, asSelfLabeledFormField } from './utils/asFormField'
 
 export { TextInput }
 export type { FieldInputComponentProps }
@@ -16,32 +16,25 @@ const TextField = asFormField(TextInput)
 const MarkdownField = asFormField(MarkdownInput)
 const Switch = asSelfLabeledFormField(SwitchInput)
 
-type ExternalFieldProps<Data, Output extends Input, Extra, Input> = FieldProps<Output, Extra, Input> & {
-  path: FieldPath<Input, Output, Data>
-}
-
-type FieldComponent<Data, Output extends Input, Extra, Input = Nullable<Output>> = (p: Omit<ExternalFieldProps<Data, Output, Extra, Input>, 'component'>) => React.ReactElement
+type FieldComponent<Data, Output extends Input, Extra, Input = Nullable<Output>> = SpecializedFieldComponent<FieldProps<Output, Extra, Input, Data>>
+type SelfLabeledFieldComponent<Data, Output extends Input, Extra extends Labelable, Input = Nullable<Output>> = SpecializedFieldComponent<SelfLabeledFieldProps<Output, Extra, Input, Data>>
 
 type FieldComponentFor<Data, Component> = Component extends ComponentType<infer Props>
   ? Props extends FieldInputComponentProps<infer Output, infer Input>
     ? FieldComponent<Data, Output, ComponentProps<Component>, Input>
-    : 0
-  : 1
-
-type ExternalSelfLabeledFieldProps<Data, Output extends Input, Extra extends Labelable, Input = Nullable<Output>> = SelfLabeledFieldProps<Output, Extra, Input> & {
-  path: FieldPath<Input, Output, Data>
-}
+    : never
+  : never
 
 interface FormFor<Data> extends FieldsFor<Data>, HooksFor<Data> {
   Form: ComponentType<FormProps<Data>>
 }
 
 interface FieldsFor<Data> {
-  Field: <Output extends Input, Extra, Input>(props: ExternalFieldProps<Data, Output, Extra, Input>) => React.ReactElement
+  Field: <Output extends Input, Extra, Input>(props: FieldProps<Output, Extra, Input, Data>) => React.ReactElement
   asFormField: <Output extends Input, Extra, Input>(c: FieldInputComponent<Output, Extra, Input>) => FieldComponent<Data, Output, Extra, Input>
   MarkdownField: FieldComponentFor<Data, typeof MarkdownInput>
   TextField: FieldComponentFor<Data, typeof TextInput>
-  Switch: (p: Omit<ExternalSelfLabeledFieldProps<Data, boolean, SwitchProps>, 'component'>) => React.ReactElement
+  Switch: SelfLabeledFieldComponent<Data, boolean, SwitchProps>
 }
 
 interface HooksFor<Data> {
