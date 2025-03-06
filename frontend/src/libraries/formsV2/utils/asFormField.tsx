bb05e2d69690dd102adf56b8df308dd92f96018c
Purr Consuegra <purr.consuegra@solita.fi>
2025-03-06 08:06:49 +0200

    More clearer types for asFormField

--- frontend/src/libraries/formsV2/utils/asFormField.tsx
+++ frontend/src/libraries/formsV2/utils/asFormField.tsx
@@ -3,25 +3,24 @@ import { Labelable } from '../types'
 import type { FieldInputComponent } from '../components/inputs'
 import { type FieldProps, type SelfLabeledFieldProps, type UnwrappedFieldProps, Field, SelfLabeledField, UnwrappedField } from '../Field'
 
+export type SpecializedFieldComponent<P> = (props: Omit<P, 'component'>) => React.ReactElement
+
 export function asFormField<Output extends Input, Extra, Input>(
-  c: FieldInputComponent<Output, Extra, Input>): (props: Omit<FieldProps<Output, Extra, Input>, 'component'>
-) => React.ReactElement {
+  c: FieldInputComponent<Output, Extra, Input>): SpecializedFieldComponent<FieldProps<Output, Extra, Input>> {
   return props => {
     return <Field {...props as FieldProps<Output, Extra, Input>} component={c} />
   }
 }
 
 export function asUnwrappedFormField<Output extends Input, Extra, Input>(
-  c: FieldInputComponent<Output, Extra, Input>): (props: Omit<UnwrappedFieldProps<Output, Extra, Input>, 'component'>
-) => React.ReactElement {
+  c: FieldInputComponent<Output, Extra, Input>): SpecializedFieldComponent<UnwrappedFieldProps<Output, Extra, Input>> {
   return props => {
     return <UnwrappedField {...props as UnwrappedFieldProps<Output, Extra, Input>} component={c} />
   }
 }
 
 export function asSelfLabeledFormField<Output extends Input, Extra extends Labelable, Input>(
-  c: FieldInputComponent<Output, Extra & {label: string}, Input>): (props: Omit<SelfLabeledFieldProps<Output, Extra, Input>, 'component'>
-) => React.ReactElement {
+  c: FieldInputComponent<Output, Extra & {label: string}, Input>): SpecializedFieldComponent<SelfLabeledFieldProps<Output, Extra, Input>> {
   return props => {
     return <SelfLabeledField<Output, Extra, Input> {...props as SelfLabeledFieldProps<Output, Extra, Input>} component={c} />
   }
