aacd677c9ec19c2aaf7a1ae693b06ff6a59cddc6
Purr Consuegra <purr.consuegra@solita.fi>
2025-03-08 13:44:44 +0200

    Move validation to own reducer

---
+++ frontend/src/libraries/formsV2/reducer/validationReducer.ts
@@ -0,0 +1,36 @@
+import { ValidationAction } from './types'
+import type { Errors } from '../types'
+
+import { assoc, dissoc } from './utils'
+
+export type ErrorMap = Partial<Record<string, Errors>>
+export interface FormValidationState {
+  errors: ErrorMap
+  isValid: boolean
+}
+
+export const initialValidationState : FormValidationState = {
+  errors: {},
+  isValid: true,
+}
+
+export function validationReducer(state: FormValidationState, action: ValidationAction): FormValidationState {
+  const { id, errors } = action
+  const errorMap = errors === undefined
+    ? dissoc(state.errors, id)
+    : assoc(state.errors, id, errors)
+
+  if (errorMap === state.errors) return state
+
+  return {
+    errors: errorMap,
+    isValid: isValid(errorMap),
+  }
+}
+
+function isValid(map: ErrorMap): boolean {
+  for (const _ in map) {
+    return false
+  }
+  return true
+}
