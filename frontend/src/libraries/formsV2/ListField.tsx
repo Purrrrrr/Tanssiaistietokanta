6a79f25c99907fdad4782f1a898e113325be9153
Purr Consuegra <purr.consuegra@solita.fi>
2025-04-04 22:16:40 +0300

    Better typing

--- frontend/src/libraries/formsV2/ListField.tsx
+++ frontend/src/libraries/formsV2/ListField.tsx
@@ -1,34 +1,27 @@
-import type { AnyType, FieldPath, ValidationProps } from './types'
+import type { AnyType, FieldPath } from './types'
 
 import { type ConnectedFieldProps, ConnectedInput } from './components/ConnectedInput'
-import { type ExternalBareFieldContainerProps, BareFieldContainer } from './components/FieldContainer'
-import type { FieldInputComponent, OmitInputProps } from './components/inputs'
 import { type ListItem, Repeater } from './components/Repeater'
 
-export type ListFieldProps<Output extends Input, Extra, Input extends ListItem, Data = AnyType> = ValidationProps &
-  ExternalBareFieldContainerProps &
-  OmitInputProps<Extra> & {
-    component: FieldInputComponent<Output, Extra, Input>
+export type ListFieldProps<Output extends Input & ListItem, Extra, Input, Data = AnyType> =
+  ConnectedFieldProps<Output, Extra, Input, Data> & {
+    label: string
     path: FieldPath<Output[], Output[], Data>
   }
 
-export type ListFieldProps2<Output extends Input, Extra, Input extends ListItem, Data = AnyType> =
-  ExternalBareFieldContainerProps & ConnectedFieldProps<Output, Extra, Input, Data>
-
-export function ListField<Output extends Input, Extra, Input extends ListItem, Data = AnyType>({path, label, component: C, required, schema, ...extra}: ListFieldProps<Output, Extra, Input, Data>) {
-  return <BareFieldContainer labelFor="" label={label}>
+export function ListField<Output extends Input & ListItem, Extra, Input, Data = AnyType>({label, path, component, ...extra}: ListFieldProps<Output, Extra, Input, Data>) {
+  return <fieldset>
+    <legend>{label}</legend>
     <Repeater path={path}>
       {({ id, index }) =>
         <>
-          <ConnectedInput
+          <ConnectedInput<Output, Extra, Input, Data>
             id={String(id)}
-            path={`${path}.${index}`}
-            component={C}
-            required={required}
-            schema={schema}
-            {...extra as Extra} />
+            path={`${path}.${index}` as FieldPath<Input, Output, Data>}
+            component={component}
+            {...extra as Extra & Omit<ConnectedFieldProps<Output, Extra, Input, Data>, 'path' | 'component'>} />
         </>
       }
     </Repeater>
-  </BareFieldContainer>
+  </fieldset>
 }
--- frontend/src/libraries/formsV2/foo.tsx
+++ frontend/src/libraries/formsV2/foo.tsx
@@ -8,6 +8,7 @@ interface Data {
   d?: string
   b: string | null
   bo?: boolean
+  s: string[]
   l: L[]
   l2: L[]
 }
@@ -21,21 +22,22 @@ const {
 } = formFor<Data>()
 
 export default function Foo() {
-  const [value, onChange] = useState<Data>({a: '', b: '', l: [{_id: '1', value: ''}, {_id: '2', value: ''}], l2: [{_id: '3', value: ''}, {_id: '4', value: ''}]})
+  const [value, onChange] = useState<Data>({a: '', b: '', l: [{_id: '1', value: ''}, {_id: '2', value: ''}], l2: [{_id: '3', value: ''}, {_id: '4', value: ''}], s: ['aa', 'bb']})
   return <Form value={value} onChange={onChange}>
     <h2>FOO</h2>
     <Field label="aaa" path="a" required component={TextInput} />
     <TextField label="aaa" path="b" />
     <Switch path="bo" label="Is it on?" />
     <MarkdownField path="d" label="markdooown" />
-    <ListField<L, unknown, L, Data> path="l"
+    <ListField path="s" label="Strings" component={TextInput} />
+    <ListField path="l" label="l"
       component={
         ({value, onChange}: {value: L, onChange: (l: L) => unknown}) =>
           <input type="text" value={value.value} onChange={e => onChange({ ...value, value: e.target.value})} />
       }
     />
     ---
-    <ListField<L, unknown, L, Data> path="l2"
+    <ListField path="l2" label="l 2"
       component={
         ({value, onChange}: {value: L, onChange: (l: L) => unknown}) =>
           <input type="text" value={value.value} onChange={e => onChange({ ...value, value: e.target.value})} />
