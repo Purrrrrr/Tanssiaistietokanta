8583fe27fd6ebb029042230c11a86c84ce49e5a8
Purr Consuegra <purr.consuegra@solita.fi>
2025-04-08 21:05:30 +0300

    Read only mode, ability to create a repeatable table

--- frontend/src/libraries/formsV2/components/Repeater/Repeater.tsx
+++ frontend/src/libraries/formsV2/components/Repeater/Repeater.tsx
@@ -12,6 +12,7 @@ import { SortableItem } from './SortableItem'
 export type RepeaterProps<Value extends ListItem, Data = AnyType> = {
   children: (props: ChildCallbackProps<Value, Data>) => ReactNode
   path: DataPath<Value[], Data>
+  table?: boolean
 }
 
 interface ChildCallbackProps<Value, Data> {
@@ -25,21 +26,37 @@ interface ChildCallbackProps<Value, Data> {
 const getId = (value: ListItem) => typeof value === 'object'
   ? value._id : value
 
-export function Repeater<Value extends ListItem, Data = AnyType>({path, children }: RepeaterProps<Value, Data>) {
+export function Repeater<Value extends ListItem, Data = AnyType>({path, children, table }: RepeaterProps<Value, Data>) {
   const { readOnly, getValueAt } = useFormContext<Data>()
   const values = getValueAt<Value[]>(path)
   const fieldId = useId()
   const items = useItems(fieldId, path, values)
   const ids = items.map(item => item.id)
 
+  const Wrapper = table ? 'table' : 'div'
+  const Item = table ? 'tr' : 'div'
+
+  if (readOnly) {
+    return <Wrapper>
+      {
+        items.map((data) =>
+          <Item key={data.id}>
+            {children?.({ dragHandle: null, path, id: data.id, index: data.index, value: data.value })}
+          </Item>
+        )
+      }
+    </Wrapper>
+  }
+
   return <Droppable
+    asElement={Wrapper}
     data={ { type: 'droppable', path, fieldId } satisfies DroppableData}
     disabled={values.length > 0}
   >
     <SortableContext id={fieldId} items={ids} strategy={verticalListSortingStrategy}>
       {
         items.map((data) =>
-          <SortableItem id={data.id} data={data} key={data.id}>
+          <SortableItem id={data.id} data={data} key={data.id} asElement={Item}>
             {dragHandle =>
               children?.({ dragHandle, path, id: data.id, index: data.index, value: data.value })}
           </SortableItem>
