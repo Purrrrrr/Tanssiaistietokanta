6250d4bb059c2f3dab835b7bf5cab20366438f48
Purr Consuegra <purr.consuegra@solita.fi>
2025-05-06 23:26:14 +0300

    Hooks to remove and add from lists

--- frontend/src/libraries/formsV2/foo.tsx
+++ frontend/src/libraries/formsV2/foo.tsx
@@ -20,13 +20,26 @@ interface L {
 
 const {
   Form, Field, TextField, Switch, MarkdownField, RepeatingField,
-  RepeatingSection, RepeatingTableRows
+  RepeatingSection, RepeatingTableRows,
+  useAddItem, useAddItemAt, useRemoveItem, useRemoveItemAt,
 } = formFor<Data, {'l': L, 's': string}>()
 
 export default function Foo() {
   const [value, onChange] = useState<Data>({a: '', b: '', l: [{_id: '1', value: 'a'}, {_id: '2', value: 'b'}, {_id: '5', value: 'a2'}, {_id: '6', value: 'b2'}], l2: [{_id: '3', value: 'c'}, {_id: '4', value: 'd'}], s: ['aa', 'bb']})
+
   return <Form value={value} onChange={onChange}>
     <h2>FOO</h2>
+    <FooContents />
+  </Form>
+}
+
+function FooContents() {
+  const addToL = useAddItemAt('l')
+  const addItem = useAddItem()
+  const removeFromL = useRemoveItemAt('l')
+  const removeItem = useRemoveItem()
+
+  return <>
     <Field label="aaa" path="a" required component={TextInput} />
     <RepeatingField path="s" label="Strings" component={TextInput} />
     <TextField label="aaa" path="b" />
@@ -41,6 +54,7 @@ export default function Foo() {
         </Flex>
       }
     </RepeatingSection>
+    <Button intent="primary" icon="edit" onClick={() => addItem('l', { _id: id(), value: '' })} />
     <RepeatingSection<L> path="l2" label="l 2" accepts="l" itemType="l">
       {({ dragHandle, path, index, onRemove }) =>
         <Flex>
@@ -50,6 +64,7 @@ export default function Foo() {
         </Flex>
       }
     </RepeatingSection>
+    <Button intent="primary" icon="edit" onClick={() => addItem('l2', { _id: id(), value: '' })} />
     <table>
       <RepeatingTableRows<L> path="l">
         {({ dragHandle, value, onRemove }) =>
@@ -64,5 +79,10 @@ export default function Foo() {
         }
       </RepeatingTableRows>
     </table>
-  </Form>
+  </>
+}
+
+let i = 0
+function id() {
+  return `id-${i++}`
 }
