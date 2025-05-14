3f0acecc0bb6c57d675f2a22110eb1a945eb653a
Purr Consuegra <purr.consuegra@solita.fi>
2025-05-14 17:00:55 +0300

    Readonly prop for datepicker

--- frontend/src/libraries/formsV2/components/inputs/DateRangeInput.tsx
+++ frontend/src/libraries/formsV2/components/inputs/DateRangeInput.tsx
@@ -5,5 +5,11 @@ import { FieldInputComponentProps } from './types'
 
 export type DateRangeInputProps = FieldInputComponentProps<[Date | null, Date | null]>
 
-export const DateRangeInput = ({id, value, onChange}: DateRangeInputProps) =>
-  <DateRangePicker id={id} locale="fi-FI" value={value} onChange={v => onChange(Array.isArray(v) ? v : [v, null])}/>
+export const DateRangeInput = ({id, value, onChange, readOnly}: DateRangeInputProps) =>
+  <DateRangePicker
+    id={id}
+    locale="fi-FI"
+    value={value}
+    onChange={v => onChange(Array.isArray(v) ? v : [v, null])}
+    disabled={readOnly}
+  />
