cc1ba8c1255c5a56e4a190ed58303a08e9975817
Purr Consuegra <purr.consuegra@solita.fi>
2025-05-18 21:32:50 +0300

    Remove unneeded css and imports

--- frontend/src/libraries/ui/index.tsx
+++ frontend/src/libraries/ui/index.tsx
@@ -21,7 +21,7 @@ export * from './AutosizedSection'
 export { Breadcrumb, BreadcrumbContext, Breadcrumbs } from './Breadcrumbs'
 export * from './Flex'
 export type { ButtonProps, MenuItemProps } from '@blueprintjs/core'
-export { AnchorButton, Callout, Collapse, H2, HTMLTable, Menu, MenuItem, Navbar, NonIdealState, NumericInput, ProgressBar, Section, SectionCard, Spinner, Tab, Tabs, Tag, Tooltip } from '@blueprintjs/core'
+export { AnchorButton, Callout, Collapse, H2, HTMLTable, Menu, MenuItem, Navbar, NonIdealState, ProgressBar, SectionCard, Spinner, Tab, Tabs, Tag } from '@blueprintjs/core'
 export const CssClass = {
   formGroupInline: 'formgroup-inline',
   formGroupInlineFill: 'formgroup-inline-fill',
