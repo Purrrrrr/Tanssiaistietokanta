f38488552d51146a89735c0b74e8afd3f6926d2e
Purr Consuegra <purr.consuegra@solita.fi>
2025-05-27 12:42:54 +0300

    Remove redundant css

--- frontend/src/libraries/formsV2/components/containers/FieldContainer/HiddenLabel.tsx
+++ frontend/src/libraries/formsV2/components/containers/FieldContainer/HiddenLabel.tsx
@@ -1,13 +1,11 @@
 import { LabelProps } from '../types'
 
-import './HiddenLabel.scss'
-
 interface HiddenLabelProps extends LabelProps {
   labelFor: string
 }
 
 export function HiddenLabel({label, labelFor, labelInfo}: HiddenLabelProps) {
-  return <label htmlFor={labelFor} className="hidden-label">
+  return <label htmlFor={labelFor} className="sr-only">
     {label}{labelInfo ? ' ' : ''}{labelInfo}
   </label>
 }
