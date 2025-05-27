67d226676c37ff6f20535df74b2866d5f067caf1
Purr Consuegra <purr.consuegra@solita.fi>
2025-05-27 14:40:11 +0300

    Sort classes

--- frontend/src/components/widgets/PrintViewToolbar.tsx
+++ frontend/src/components/widgets/PrintViewToolbar.tsx
@@ -14,14 +14,14 @@ export default function PrintViewToolbar({children, icon = 'settings', maxHeight
 
   return <div
     className={classNames(
-      'flex justify-center print:hidden transition-[max-height] duration-500 bg-white shadow-[0_0_10px_rgba(0,0,0,0.5)] border-b border-b-gray-400',
-      isOpen ? 'open max-h-20' : 'max-h-0',
+      'flex justify-center bg-white border-b duration-500 print:hidden transition-[max-height] shadow-[0_0_10px_rgba(0,0,0,0.5)] border-b-gray-400',
+      isOpen ? 'max-h-20' : 'max-h-0',
     )}
     style={isOpen && maxHeight ? {maxHeight} : undefined}
   >
     <button
       className={classNames(
-        'more absolute right-3 top-3 p-2 hover:bg-white/50 rounded-full transition-[background,_opacity]',
+        'absolute top-3 right-3 p-2 rounded-full transition-[background,_opacity] hover:bg-white/50',
         isOpen ? 'opacity-0' : 'opacity-80 delay-[0ms,_400ms]',
       )}
       onFocus={() => setOpen(true)}
@@ -30,7 +30,7 @@ export default function PrintViewToolbar({children, icon = 'settings', maxHeight
     </button>
     <div
       className={classNames(
-        'print-toolbar-contents p-2 max-h-20 max-w-[800px] transition overflow-y-auto',
+        'overflow-y-auto p-2 max-h-20 transition max-w-[800px]',
         isOpen || 'opacity-0 -translate-y-full'
       )}
       style={maxHeight ? {maxHeight} : undefined}
@@ -39,7 +39,7 @@ export default function PrintViewToolbar({children, icon = 'settings', maxHeight
     </div>
     <button
       className={classNames(
-        'close absolute right-3 top-1 p-2 hover:bg-gray-200',
+        'absolute top-1 right-3 p-2 hover:bg-gray-200',
         isOpen ? 'transform-none' : 'transition opacity-0 -translate-y-full'
       )}
       onClick={() => setOpen(false)}
