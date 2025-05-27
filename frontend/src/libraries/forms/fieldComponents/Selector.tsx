b53a3f8c459e74d9374e8f30eede0ee0301ec769
Purr Consuegra <purr.consuegra@solita.fi>
2025-05-27 14:40:42 +0300

    Remove unused selector code

--- frontend/src/libraries/forms/fieldComponents/Selector.tsx
+++ frontend/src/libraries/forms/fieldComponents/Selector.tsx
@@ -1,32 +1,10 @@
-import {useState} from 'react'
 import { Popover } from '@blueprintjs/core'
-import { ItemRenderer as BPItemRenderer, QueryList, QueryListProps} from '@blueprintjs/select'
 
-import { Button, ButtonProps, MenuItem, SearchBar } from 'libraries/ui'
+import { Button, ButtonProps } from 'libraries/ui'
 
 import { ActionButton } from '../formControls'
 
 import  './Selector.scss'
-
-type SelectorProps<T> = SelectorMenuProps<T> & {
-  text: string
-  buttonProps?: Omit<ButtonProps, 'text'>
-  alwaysEnabled?: boolean
-}
-export function Selector<T>({text, buttonProps, onSelect, alwaysEnabled, ...rest} : SelectorProps<T>) {
-  const [open, setOpen] = useState(false)
-  return <MenuButton
-    menu={
-      <SelectorMenu onSelect={(item) => { setOpen(false); onSelect(item)}} {...rest} />
-    }
-    text={text}
-    buttonProps={buttonProps}
-    open={open}
-    onSetOpen={setOpen}
-    alwaysEnabled={alwaysEnabled}
-  />
-}
-
 
 interface MenuButtonProps{
   open?: boolean
@@ -64,54 +42,6 @@ function onSelectOpen(e: HTMLElement) {
     }
   }
 
-  const itemList = e.querySelector('.selector-item-list > input, .selector-item-list') as HTMLElement
-  const input = itemList?.querySelector?.('input');
-  (input ?? itemList)?.focus()
-}
-
-type SelectorMenuProps<T> = {
-  items: T[] | (() => T[])
-  itemPredicate?: QueryListProps<T>['itemPredicate']
-  selectedItem?: T
-  onSelect: (t: T) => unknown
-  getItemText: ((t: T) => string | JSX.Element) | null
-  itemRenderer?: ItemRenderer<T>
-} & (
-{
-  filterable: true
-  searchPlaceholder: string
-  emptySearchText: string
-} | {
-  filterable?: false
-  searchPlaceholder?: string
-  emptySearchText?: string
-}
-
-)
-type ItemRenderer<T> = (text: string | JSX.Element | undefined, ...rest: Parameters<BPItemRenderer<T>>) => ReturnType<BPItemRenderer<T>>
-function SelectorMenu<T>({items, itemPredicate, selectedItem, getItemText, onSelect, filterable, searchPlaceholder, emptySearchText, itemRenderer = defaultItemRenderer} : SelectorMenuProps<T>) {
-  const [query, setQuery] = useState('')
-  return <QueryList<T>
-    query={query}
-    initialActiveItem={selectedItem}
-    items={unwrap(items)}
-    itemPredicate={itemPredicate}
-    renderer={({className, itemList, handleKeyUp, handleKeyDown}) =>
-      <div className={'selector-item-list '+className} tabIndex={0} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown}>
-        {filterable && <SearchBar id="selector-query" value={query} onChange={setQuery} placeholder={searchPlaceholder} emptySearchText={emptySearchText} />}
-        {itemList}
-      </div>
-    }
-    itemRenderer={(item, ...args) => itemRenderer(getItemText?.(item), item, ...args)}
-    onItemSelect={onSelect}
-  />
-}
-
-const defaultItemRenderer : ItemRenderer<unknown> = function defaultItemRenderer(text, item, {handleClick, index, modifiers: {active}}) {
-  return <MenuItem key={index} roleStructure="listoption" text={text} onClick={handleClick} active={active} />
-}
-
-function unwrap<T>(items: T[] | (() => T[])): T[] {
-  if (typeof items === 'function') return items()
-  return items
+  const input = e.querySelector?.('input')
+  input?.focus()
 }
