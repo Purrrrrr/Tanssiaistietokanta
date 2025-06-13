447545cb423ed9541767c9250916b33aa0dc10b2
Purr Consuegra <purr.consuegra@solita.fi>
2025-06-13 11:54:42 +0300

    Replace blueprint dropdown with our own in ConflictHandler

--- frontend/src/libraries/forms/ConflictHandler.tsx
+++ frontend/src/libraries/forms/ConflictHandler.tsx
@@ -1,9 +1,8 @@
 import { ConflictData } from './types'
 
-import MenuButton2 from 'libraries/formsV2/components/MenuButton'
+import MenuButton from 'libraries/formsV2/components/MenuButton'
 import {Button, Flex} from 'libraries/ui'
 
-import {MenuButton} from './fieldComponents/Selector'
 import {useFormStrings} from './formContext'
 
 export function ConflictHandler({localValue, serverValue, onResolve}: ConflictData) {
@@ -11,30 +10,6 @@ export function ConflictHandler({localValue, serverValue, onResolve}: ConflictDa
   return <>
     {strings.hasConflicts}{' '}
     <MenuButton
-      alwaysEnabled
-      menu={
-        <Flex spaced style={{padding: '1em'}}>
-          <div>
-            {strings.server}
-            {serverValue}
-            <Button intent="primary" text={strings.chooseThis} onClick={() => onResolve('SERVER')} />
-          </div>
-          <div>
-            {strings.local}
-            {localValue}
-            <Button intent="primary" text={strings.chooseThis} onClick={() => onResolve('LOCAL')} />
-          </div>
-        </Flex>
-      }
-      text={strings.conflictButton}
-      buttonProps={{
-        rightIcon: 'caret-down',
-        outlined: true,
-        small: true,
-        intent: 'danger',
-      }}
-    />
-    <MenuButton2
       text={strings.conflictButton}
       buttonProps={{
         rightIcon: 'caret-down',
@@ -55,6 +30,6 @@ export function ConflictHandler({localValue, serverValue, onResolve}: ConflictDa
           <Button intent="primary" text={strings.chooseThis} onClick={() => onResolve('LOCAL')} />
         </div>
       </Flex>
-    </MenuButton2>
+    </MenuButton>
   </>
 }
