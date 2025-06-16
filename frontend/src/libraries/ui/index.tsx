8d71fbd64549d496bb972b8d1eb903c7c043a286
Purr Consuegra <purr.consuegra@solita.fi>
2025-06-16 15:23:47 +0300

    Removed duplicate Icon impl

--- frontend/src/libraries/ui/index.tsx
+++ frontend/src/libraries/ui/index.tsx
@@ -4,10 +4,8 @@ import {
   Classes,
   FormGroup as BlueprintFormGroup,
   FormGroupProps as BlueprintFormGroupProps,
-  Icon as BlueprintIcon,
   InputGroup as BlueprintInputGroup,
 } from '@blueprintjs/core'
-import { IconPaths, Icons } from '@blueprintjs/icons'
 import classNames from 'classnames'
 
 import { Button } from './Button'
@@ -20,6 +18,7 @@ export { type AnchorButtonProps, type ButtonProps, AnchorButton, Button } from '
 export { Collapse } from './Collapse'
 export * from './Flex'
 export { GlobalSpinner } from './GlobalLoadingSpinner'
+export * from './Icon'
 export { Markdown } from './Markdown'
 export { TagButton } from './Tag'
 export { Callout, H2, HTMLTable, ProgressBar, SectionCard, Tab, Tabs } from '@blueprintjs/core'
@@ -33,98 +32,6 @@ export const CssClass = {
 
 type HTMLDivProps = React.HTMLAttributes<HTMLDivElement>;
 export type Intent = 'none' | 'primary' | 'success' | 'warning' | 'danger';
-export type IconName =
-  'arrow-left'
-  | 'caret-down'
-  | 'chevron-left'
-  | 'chevron-right'
-  | 'cross'
-  | 'double-caret-vertical'
-  | 'double-chevron-up'
-  | 'download'
-  | 'edit'
-  | 'error'
-  | 'history'
-  | 'info-sign'
-  | 'link'
-  | 'move'
-  | 'music'
-  | 'outdated'
-  | 'refresh'
-  | 'saved'
-  | 'search'
-  | 'settings'
-  | 'style'
-  | 'time'
-  | 'trash'
-
-const iconModules = {
-  'arrow-left/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/arrow-left'),
-  'arrow-left/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/arrow-left'),
-  'caret-down/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/caret-down'),
-  'caret-down/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/caret-down'),
-  'chevron-left/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/chevron-left'),
-  'chevron-left/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/chevron-left'),
-  'chevron-right/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/chevron-right'),
-  'chevron-right/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/chevron-right'),
-  'cross/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/cross'),
-  'cross/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/cross'),
-  'double-caret-vertical/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/double-caret-vertical'),
-  'double-caret-vertical/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/double-caret-vertical'),
-  'double-chevron-up/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/double-chevron-up'),
-  'double-chevron-up/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/double-chevron-up'),
-  'download/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/download'),
-  'download/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/download'),
-  'edit/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/edit'),
-  'edit/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/edit'),
-  'error/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/error'),
-  'error/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/error'),
-  'history/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/history'),
-  'history/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/history'),
-  'info-sign/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/info-sign'),
-  'info-sign/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/info-sign'),
-  'link/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/link'),
-  'link/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/link'),
-  'move/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/move'),
-  'move/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/move'),
-  'music/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/music'),
-  'music/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/music'),
-  'outdated/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/outdated'),
-  'outdated/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/outdated'),
-  'refresh/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/refresh'),
-  'refresh/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/refresh'),
-  'saved/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/saved'),
-  'saved/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/saved'),
-  'search/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/search'),
-  'search/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/search'),
-  'settings/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/settings'),
-  'settings/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/settings'),
-  'style/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/style'),
-  'style/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/style'),
-  'time/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/time'),
-  'time/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/time'),
-  'trash/16': () => import('@blueprintjs/icons/lib/esm/generated/16px/paths/trash'),
-  'trash/20': () => import('@blueprintjs/icons/lib/esm/generated/20px/paths/trash'),
-}
-
-Icons.setLoaderOptions({
-  loader: async (name, size) =>
-    (await iconModules[`${name}/${size}`]()).default as IconPaths,
-})
-
-interface IconProps {
-  className?: string
-  title?: string
-  icon: IconName
-  color?: string
-  iconSize?: number
-  intent?: Intent
-  onClick?: React.MouseEventHandler<HTMLElement>
-}
-
-export function Icon(props : IconProps) {
-  return <BlueprintIcon {...props} />
-}
 
 type CardProps = Omit<HTMLDivProps, 'onClick'>
 
