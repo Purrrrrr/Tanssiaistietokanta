b79ac11b0eb011d04cc9e7d4f516dfab947645c9
Purr Consuegra <purr.consuegra@solita.fi>
2025-06-16 10:51:19 +0300

    Component for UI showcase settings

---
+++ frontend/src/libraries/ui-showcase/Showcase.tsx
@@ -0,0 +1,82 @@
+import { ReactNode, useId, useState } from 'react'
+
+import { Switch, TextInput } from 'libraries/formsV2/components/inputs'
+import { Card } from 'libraries/ui'
+
+interface ShowcaseProps<P extends PropDefs> {
+  title: string
+  propDefs: P
+  children: (props: PropTypes<P>) => ReactNode
+}
+
+export type PropDefs = Record<string, PropDef>
+type PropDef =
+  | { type: 'string', default?: string }
+  | { type: 'boolean', default?: boolean }
+  | { type: 'number', default?: number }
+
+type PropTypes<P extends PropDefs> = {
+  [K in keyof P]: PropType<P[K]>
+}
+type PropType<P extends PropDef> = P['default']//Exclude<P['default'], undefined>
+
+export function Showcase<P extends PropDefs>(showcase: ShowcaseProps<P>) {
+  const { title, propDefs, children } = showcase
+  const [props, setProps] = useState<PropTypes<P>>(() =>
+    Object.fromEntries(
+      Object.entries(propDefs).map(([name, def]) => {
+        return [name, defaultValue(def)]
+      })
+    ) as PropTypes<P>
+  )
+  const fields = Object.entries(propDefs).map(([name, def]) =>
+    <PropField key={name} label={name} def={def} value={props[name]} onChange={value => setProps(old => ({...old, [name]: value}))} />
+  )
+
+  return <Card>
+    <h2>{title}</h2>
+    <div className="flex justify-between items-center">
+      <div className="p-2">{children(props)}</div>
+      {fields.length > 0 && <div className="border-l-1 border-stone-300 p-2 pl-7">{fields}</div>}
+    </div>
+  </Card>
+
+}
+
+function defaultValue(def: PropDef) {
+  switch(def.type) {
+    case 'string':
+      return def.default ?? ''
+    case 'boolean':
+      return def.default ?? false
+    case 'number':
+      return def.default ?? 0
+  }
+}
+
+interface PropFieldProps<P extends PropDef> {
+  def: P
+  label: string
+  value: PropType<P>
+  onChange: (value: PropType<P>) => unknown
+}
+
+function PropField<P extends PropDef>({def, label, value, onChange}: PropFieldProps<P>) {
+  const id = useId()
+  switch(def.type) {
+    case 'string':
+      return <div>
+        <label htmlFor={id}>{label}</label>
+        <TextInput id={id} value={value as string} onChange={onChange} />
+      </div>
+    case 'boolean':
+      return <div>
+        <Switch id={id} label={label} value={value as boolean} onChange={onChange} />
+      </div>
+    case 'number':
+      return <div>
+        <label htmlFor={id}>{label}</label>
+        <input type="number" id={id} value={value as number} onChange={e => onChange(parseFloat(e.target.value))} />
+      </div>
+  }
+}
--- frontend/src/libraries/ui-showcase/index.tsx
+++ frontend/src/libraries/ui-showcase/index.tsx
@@ -1,27 +1,50 @@
-import classNames from 'classnames'
-
 import FormUiShowcase from 'libraries/formsV2/UiShowcase'
-import { Button } from 'libraries/ui'
+import { Button, Tab, Tabs } from 'libraries/ui'
 
+import { Showcase } from './Showcase'
 import { titleCase } from './utils/titleCase'
 
 const colors = ['none', 'primary', 'success', 'danger', 'warning'] as const
 
 export default function UiShowcase() {
   return <section>
-    <section className="p-2 bg-amber-50 h-100">
-      <h1>Colors</h1>
+    <section className="">
+      <h1>UI Showcase</h1>
 
       <a href="#">
         Link
       </a>
 
-      {colors.map(color =>
-        <p><Button intent={color} icon="trash">{titleCase(color)}</Button></p>
-      )}
+      <Showcase
+        title="Button"
+        propDefs={{
+          disabled: { type: 'boolean' },
+          icon: { type: 'boolean', default: true },
+        }}
+      >
+        {({ disabled, icon }) =>
+          <div className="flex gap-2">
+            {colors.map(color =>
+              <Button key={color} intent={color} icon={icon ? 'trash' : undefined} disabled={disabled as boolean} >{titleCase(color)}</Button>
+            )}
+          </div>
+        }
+      </Showcase>
+      <Showcase
+        title="Tabs"
+        propDefs={{
+        }}
+      >
+        {() =>
+          <Tabs defaultSelectedTabId="tab2">
+            <Tab id="tab1" title="Tab1" panel={<p>Panel 1</p>} />
+            <Tab id="tab2" title="Tab2" panel={<p>Panel 2</p>} />
+            <Tab id="tab3" title="Tab3" panel={<p>Panel 3</p>} />
+          </Tabs>
+        }
+      </Showcase>
     </section>
 
     <FormUiShowcase />
   </section>
-
 }
