cf64ffb9d39a25a8a9b5f0d5387ff4eccdb82c82
Purr Consuegra <purr.consuegra@solita.fi>
2025-07-08 21:43:40 +0300

    Fix weird time format when displaying times like 1:09.99 as '1:010'

--- frontend/src/utils/duration.tsx
+++ frontend/src/utils/duration.tsx
@@ -1,6 +1,7 @@
-export function toMinSec(value : number) {
-  const seconds = value%60
-  const minutes = Math.floor(value/60)
+export function toMinSec(value: number) {
+  const rounded = Math.round(value)
+  const seconds = rounded%60
+  const minutes = Math.floor(rounded/60)
   return [minutes, seconds]
 }
 
@@ -8,7 +9,7 @@ export function toSeconds(minutes : number, seconds : number) {
   return Math.max(seconds+(minutes*60), 0)
 }
 
-export const prefixZero = (value: number) => (value < 10 ? '0' : '') + value.toFixed()
+export const prefixZero = (value: number) => (value < 10 ? '0' : '') + value
 
 export function durationToString(value : number | undefined) {
   if (!value || value < 0) return '0:00'
