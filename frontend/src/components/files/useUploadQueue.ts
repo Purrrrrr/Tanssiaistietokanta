602b7c88f951b65ea338e6f2c4419a7c989c325f
Purr Consuegra <purr.consuegra@solita.fi>
2025-10-14 20:21:13 +0300

    useQueue hook

--- frontend/src/components/files/useUploadQueue.ts
+++ frontend/src/components/files/useUploadQueue.ts
@@ -1,48 +1,38 @@
-import { useState } from 'react'
-
 import { type Progress, doUpload  } from 'services/files'
+
+import { useQueue } from 'libraries/i18n/useQueue'
 
 import { useGetUploadErrorMessage } from './useGetUploadErrorMessage'
 
 interface Upload {
-  id: number
   file: File
   abort: () => unknown
   error?: string
   progress?: Progress
 }
 
-let idCounter = 0
-
 export function useUploadQueue(root: string = '', path: string = '') {
-  const [uploads, setUploads] = useState<Upload[]>([])
+  const [uploads, queue] = useQueue<Upload>()
   const getError = useGetUploadErrorMessage()
 
   const upload = async (file: File, fileId?: string) => {
     const abortController = new AbortController()
-    const id = ++idCounter
     const abort = () => {
+      queue.remove(id)
       abortController.abort()
-      setUploads(uploads => uploads.filter(u => u.id != id))
     }
-    const updateState = (state: Partial<Upload>) => setUploads(uploads =>
-      uploads.map(u => u.id === id ? { ...u, ...state } : u)
-    )
-    setUploads(uploads => [
-      ...uploads,
-      { id, file, abort },
-    ])
+    const id = queue.push({ file, abort })
 
     try {
       const result = await doUpload({
         root, path, fileId, file,
         signal: abortController.signal,
-        onProgress: (progress) => updateState({ progress }),
+        onProgress: (progress) => queue.update(id, { progress }),
       })
       abort()
       return result
     } catch (e) {
-      updateState({ error: getError(e, file) })
+      queue.update(id, { error: getError(e, file) })
       throw e
     }
   }
---
+++ frontend/src/libraries/i18n/useQueue.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+type WithId<X> = X & {
+  id: number
+}
+
+let idCounter = 0
+
+export function useQueue<T>(initialState: WithId<T>[] = []) {
+  const [items, setItems] = useState<WithId<T>[]>(initialState)
+
+  const updateItem = (id: number, state: Partial<T>) => setItems(items =>
+    items.map(u => u.id === id ? { ...u, ...state } : u)
+  )
+  const removeItem = (id: number) => {
+    setItems(items => items.filter(u => u.id != id))
+  }
+
+  const pushItem = (item: T) => {
+    const id = ++idCounter
+    setItems(items => [
+      ...items,
+      { id, ...item },
+    ])
+    return id
+  }
+
+  return [items, {
+    update: updateItem,
+    remove: removeItem,
+    push: pushItem,
+  }] as const
+}
