4213ffc8cb11881e0c185fdcfc5bef151e9e793d
Purr Consuegra <purr.consuegra@solita.fi>
2026-01-11 00:45:11 +0200

    Clean up unused code

--- frontend/src/libraries/ui/FormGroup.tsx
+++ frontend/src/libraries/ui/FormGroup.tsx
@@ -1,43 +1,25 @@
 import React from 'react'
 import classNames from 'classnames'
 
-import { Color } from './types'
+import { ColorClass, CssClass } from './classes'
 
-import { CssClass } from './classes'
-
-export interface FormGroupProps extends Omit<FormGroupPropsB, 'inline' | 'intent'>, React.ComponentPropsWithoutRef<'div'> {
+export interface FormGroupProps extends Omit<FormGroupPropsB, 'inline' | 'color'>, React.ComponentPropsWithoutRef<'div'> {
   elementRef?: React.Ref<HTMLDivElement>
   inline?: boolean
   labelStyle?: 'above' | 'beside'
-  color?: Color
+  color?: 'danger'
 }
 
 export interface FormGroupPropsB {
   elementRef?: React.Ref<HTMLDivElement>
   className?: string
-  intent?: Color
+  color?: 'danger'
   children?: React.ReactNode
-  /**
-     * A space-delimited list of class names to pass along to the
-     * `Classes.FORM_CONTENT` element that contains `children`.
-     */
-  contentClassName?: string
-
-  /**
-     * Whether form group should appear as non-interactive.
-     * Remember that `input` elements must be disabled separately.
-     */
-  disabled?: boolean
-
-  /**
-     * Whether the component should take up the full width of its container.
-     */
-  fill?: boolean
 
   /**
      * Optional helper text. The given content will be wrapped in
      * `Classes.FORM_HELPER_TEXT` and displayed beneath `children`.
-     * Helper text color is determined by the `intent`.
+     * Helper text color is determined by the `color`.
      */
   helperText?: React.ReactNode
 
@@ -45,7 +27,7 @@ export interface FormGroupPropsB {
   inline?: boolean
 
   /** Label of this form group. */
-  label?: React.ReactNode
+  label: React.ReactNode
 
   /**
      * `id` attribute of the labelable form element that this `FormGroup` controls,
@@ -61,28 +43,22 @@ export interface FormGroupPropsB {
   /**
      * Optional text for `label`. The given content will be wrapped in
      * `Classes.FORM_GROUP_SUB_LABEL` and displayed beneath `label`. The text color
-     * is determined by the `intent`.
+     * is determined by the `color`.
      */
   subLabel?: React.ReactNode
 }
 
-export function FormGroup({ elementRef, className, inline, labelStyle: maybeLabelStyle, ...props }: FormGroupProps) {
+export function FormGroup({ elementRef, className, inline = false, labelStyle: maybeLabelStyle, ...props }: FormGroupProps) {
   const labelStyle = maybeLabelStyle ?? (inline ? 'beside' : 'above')
-  const inlineLabel = labelStyle === 'beside'
-  const inlineProps = inline
-    ? { inline: true, className: classNames(CssClass.formGroupInline, className) }
-    : { inline: inlineLabel, className: classNames(inlineLabel && CssClass.formGroupInlineFill, className) }
 
-  const {
-    color, children, disabled, contentClassName, helperText, label, labelFor, labelInfo, style, subLabel,
-    ...rest
-  } = props
-  const props2 = {
-    intent: color, children, disabled, contentClassName, helperText, label, labelFor, labelInfo, style, subLabel,
-    ...inlineProps,
-  }
-
-  return <FG {...props2} elementRef={elementRef} {...rest} />
+  return <FG
+    elementRef={elementRef}
+    {...props}
+    inline={inline || labelStyle === 'beside'}
+    className={inline
+      ? classNames(CssClass.formGroupInline, className)
+      : classNames(labelStyle === 'beside' && CssClass.formGroupInlineFill, className)}
+  />
 }
 
 const Classes = {
@@ -90,37 +66,29 @@ const Classes = {
   FORM_GROUP_SUB_LABEL: 'bp5-form-group-sub-label',
   FORM_CONTENT: 'bp5-form-content',
   FORM_HELPER_TEXT: 'bp5-form-helper-text',
-  DISABLED: 'bp5-disabled',
-  FILL: 'bp5-fill',
   INLINE: 'bp5-inline',
   LABEL: 'bp5-label',
-  TEXT_MUTED: 'bp5-text-muted',
-  intentClass: (intent?: Color) => (intent ? `bp5-intent-${intent.toLowerCase()}` : ''),
 }
 
 export function FG(props: FormGroupPropsB) {
-  const { elementRef, children, contentClassName, helperText, label, labelFor, labelInfo, subLabel, className, disabled, fill, inline, intent, ...rest } = props
+  const { elementRef, children, helperText, label, labelFor, labelInfo, subLabel, className, inline, color, ...rest } = props
   const cn = classNames(
     Classes.FORM_GROUP,
-    Classes.intentClass(intent),
     {
-      [Classes.DISABLED]: disabled,
-      [Classes.FILL]: fill,
+      'bp5-color-danger': color === 'danger',
       [Classes.INLINE]: inline,
     },
     className,
   )
   return (
     <div className={cn} {...rest} ref={elementRef}>
-      {label && (
-        <label className={Classes.LABEL} htmlFor={labelFor}>
-          {label} <span className={Classes.TEXT_MUTED}>{labelInfo}</span>
-        </label>
-      )}
-      {subLabel && <div className={Classes.FORM_GROUP_SUB_LABEL}>{subLabel}</div>}
-      <div className={classNames(Classes.FORM_CONTENT, contentClassName)}>
+      <label className={Classes.LABEL} htmlFor={labelFor}>
+        {label} <span className={ColorClass.textMuted}>{labelInfo}</span>
+      </label>
+      {subLabel && <div className={'mb-1' + Classes.FORM_GROUP_SUB_LABEL}>{subLabel}</div>}
+      <div className={Classes.FORM_CONTENT}>
         {children}
-        {helperText && <div className={Classes.FORM_HELPER_TEXT}>{helperText}</div>}
+        {helperText && <div className={'mt-1 ' + Classes.FORM_HELPER_TEXT}>{helperText}</div>}
       </div>
     </div>
   )
