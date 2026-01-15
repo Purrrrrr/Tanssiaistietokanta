0fa39702d3f59c2faf948208c11fd16821f2d1b8
Purr Consuegra <purr.consuegra@solita.fi>
2026-01-15 18:18:49 +0200

    More beautiful console logging

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -1,27 +1,26 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/logging.html
-import { MESSAGE } from 'triple-beam';
+import { MESSAGE } from 'triple-beam'
 import { createLogger, format, transports } from 'winston'
-import { colorize } from 'json-colorizer';
+import { colorize } from 'json-colorizer'
 import { 
   bold,
   redBright,
   yellow,
-  green,
   blueBright,
   magentaBright,
   type Color,
-} from 'colorette';
-import { isString, omit } from 'es-toolkit';
+} from 'colorette'
+import { isRegExp, isString, omit } from 'es-toolkit'
 
 function indent(str: string): string {
-  const indentation = ' '.repeat(2);
-  return str.replaceAll(/^/gm, indentation);
+  const indentation = ' '.repeat(2)
+  return str.replaceAll(/^/gm, indentation)
 }
 
-function formatObject(obj: { message?: unknown, level?: unknown }): string {
+function formatObject(obj: any): string {
   const data = omit(obj, ['message', 'level'])
   if (Object.values(data).filter(isString).length === 0) return ''
-  return colorize(data)
+  return colorize(JSON.stringify(data, (_: any, value: unknown) => isRegExp(value) ? String(value) : value, 2))
 }
 
 const levelColors: Record<string, Color> = {
@@ -30,43 +29,36 @@ const levelColors: Record<string, Color> = {
   info: blueBright,
   debug: magentaBright,
   default: String,
-};
+}
 
 const levelFormatter = (level: string, uppercase: boolean) => {
   const colorizeLevel = levelColors[level] ?? levelColors['default']
   return bold(colorizeLevel(uppercase ? level.toUpperCase() : level))
 }
 
-const basicColorsFormatter = format((info) => {
-  const { level, message } = info;
-  const msg = typeof message === 'string' ? message : colorize(message as string | object)
-  let output = `${levelFormatter(level, true)}: ${bold(msg)}`
-  info[MESSAGE] = output
-  return info;
-})()
-
 const cliJson = format((info) => {
-  basicColorsFormatter.transform(info)
-  const { errorStack, messages, ...rest } = info;
+  const { level, message, errorStack, messages, durationMs, method, path, timestamp, ...rest } = info
+  const msgStr = typeof message === 'string' ? message : colorize(message as string | object)
+  let msg = `${timestamp ?? new Date().toISOString()} ${levelFormatter(level, true)}: ${bold(msgStr)}`
+  if (durationMs !== undefined) msg += ` (${bold(`${durationMs}ms`)})`
 
-
-  let msg = info[MESSAGE] as string
   const json = formatObject(rest)
   if (json) msg += indent(json)
 
   if (Array.isArray(info.messages)) {
     const messages = info.messages
       .map(subInfo => {
-        return `-> ${levelFormatter(subInfo.level, false)}: ${subInfo.message} ${indent(formatObject(subInfo))}`;
+        const { time, timestamp, ...json } = subInfo
+        return `${bold(`+${time}ms`).padEnd(8)} ${levelFormatter(subInfo.level, false)}: ${subInfo.message} ${indent(formatObject(json))}`
       })
     msg += `\n${messages.map(indent).join('\n')}`
   }
   if (errorStack) {
-    msg += `\n${bold('Stack trace:')}\n${errorStack}`
+    msg += `\n${bold('  Stack trace:')}\n${errorStack}`
   }
 
   info[MESSAGE] = msg
-  return info;
+  return info
 })
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
