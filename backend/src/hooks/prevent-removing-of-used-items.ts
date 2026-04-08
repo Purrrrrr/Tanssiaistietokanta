78d53b4ad88fb5a5846777b04f3deeead339b57d
Purr Consuegra <purr.consuegra@solita.fi>
2026-04-08 14:38:17 +0300

    Change exception type to handle status code

--- backend/src/hooks/prevent-removing-of-used-items.ts
+++ backend/src/hooks/prevent-removing-of-used-items.ts
@@ -1,11 +1,11 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/hook.html
+import { Conflict } from '@feathersjs/errors'
 import type { HookContext } from '../declarations'
 import { isUsedBySomething } from '../internal-services/dependencies'
 
 export const preventRemovingOfUsedItems = async (context: HookContext) => {
   const { id, path } = context
   if (id !== undefined && isUsedBySomething(path, id)) {
-    context.statusCode = 409
-    throw new Error('The item you tried to delete is still in use')
+    throw new Conflict('The item you tried to delete is still in use')
   }
 }
