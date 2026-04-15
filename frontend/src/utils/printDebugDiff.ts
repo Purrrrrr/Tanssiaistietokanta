324852d937974e77e8a440da520375b812c6cea2
Purr Consuegra <purr.consuegra@solita.fi>
2026-04-15 17:17:52 +0300

    Debug utility to print differences in two objects

---
+++ frontend/src/utils/printDebugDiff.ts
@@ -0,0 +1,15 @@
+/** Debug utility to print differences between two objects, including nested structures. */
+export function printDebugDiff(obj1: unknown, obj2: unknown, path = ''): void {
+  if (typeof obj1 !== typeof obj2) {
+    console.log(`Type mismatch at ${path}:`, obj1, obj2)
+    return
+  }
+  if (typeof obj1 === 'object' && obj1 != null && obj2 != null) {
+    const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)])
+    for (const key of keys) {
+      printDebugDiff((obj1 as Record<string, unknown>)[key], (obj2 as Record<string, unknown>)[key], `${path}/${key}`)
+    }
+  } else if (obj1 !== obj2) {
+    console.log(`Value mismatch at ${path}:`, obj1, obj2)
+  }
+}
