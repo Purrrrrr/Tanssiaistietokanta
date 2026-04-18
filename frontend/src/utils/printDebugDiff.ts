a1a6732635b512316f276b565cbd0952569c0ee2
Purr Consuegra <purr.consuegra@solita.fi>
2026-04-18 23:04:01 +0300

    Update debug tool to show missing keys

--- frontend/src/utils/printDebugDiff.ts
+++ frontend/src/utils/printDebugDiff.ts
@@ -7,6 +7,14 @@ export function printDebugDiff(obj1: unknown, obj2: unknown, path = ''): void {
   if (typeof obj1 === 'object' && obj1 != null && obj2 != null) {
     const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)])
     for (const key of keys) {
+      if (!(key in (obj1 as Record<string, unknown>))) {
+        console.log(`Key ${key} missing in obj1 at ${path}`)
+        continue
+      }
+      if (!(key in (obj2 as Record<string, unknown>))) {
+        console.log(`Key ${key} missing in obj2 at ${path}`)
+        continue
+      }
       printDebugDiff((obj1 as Record<string, unknown>)[key], (obj2 as Record<string, unknown>)[key], `${path}/${key}`)
     }
   } else if (obj1 !== obj2) {
