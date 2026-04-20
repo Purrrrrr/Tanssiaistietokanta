5f30c79ac6ed16212b5e28ed927611af5581caef
Purr Consuegra <purr.consuegra@solita.fi>
2026-04-20 10:04:43 +0300

    Load lexical viewer and editor both lazily

--- frontend/src/libraries/lexical/index.tsx
+++ frontend/src/libraries/lexical/index.tsx
@@ -1,3 +1,7 @@
-export { DocumentViewer } from './DocumentViewer'
-export { Editor, type EditorProps } from './Editor'
+import { lazy } from 'react'
+
+export { type EditorProps } from './Editor'
 export type { MinifiedEditorState } from './utils/minify'
+
+export const DocumentViewer = lazy(() => import('./DocumentViewer').then(m => ({ default: m.DocumentViewer })))
+export const Editor = lazy(() => import('./Editor').then(m => ({ default: m.Editor })))
