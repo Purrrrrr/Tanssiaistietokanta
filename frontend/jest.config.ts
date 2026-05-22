403463b9c8c51eafc5110371253962ead6e56135
Purr Consuegra <purr.consuegra@solita.fi>
2026-05-22 11:31:53 +0300

    Fix tests

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,12 +2,16 @@ import type {Config} from 'jest'
 
 const config: Config = {
   verbose: true,
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
     '<rootDir>/cypress/',
     '<rootDir>/node_modules/',
   ],
+  setupFiles: ['<rootDir>/src/__mocks__/jestSetup.js'],
   moduleNameMapper: {
     '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^libraries/(.*)$': '<rootDir>/src/libraries/$1',
+    '\\.css$': '<rootDir>/src/__mocks__/styleMock.js',
   },
 }
 
