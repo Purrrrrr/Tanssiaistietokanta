7aa5f59a8378286d126831ae6928cbe114593d10
Purr Consuegra <purr.consuegra@solita.fi>
2026-07-11 14:59:06 +0300

    Fix a bug in withEntity helper adding _id to $select query every time it ran

--- backend/src/hooks/withEntity.ts
+++ backend/src/hooks/withEntity.ts
@@ -4,11 +4,17 @@ import { ServiceName } from '../services/access/access.schema'
 import { ServiceEntity, ServiceParams } from '../services/access/types'
 
 export function withEntity<N extends ServiceName, Result>(serviceName: N, params: null | ServiceParams<N>, callback: (entity: ServiceEntity<N>) => Result) {
+  const select = params?.query?.$select
   return async ({ app, id }: HookContext) => {
     if (!id) {
       throw new Error('ID is required')
     }
     const service = app.service(serviceName)
+
+    if (params?.query?.$select && select) {
+      // The version of feathers adapter-commons that feathers-nedb uses has a bug that causes it to mutate the $select array, so we need to make a copy of it
+      params.query.$select = select.slice()
+    }
     const entity = await service.get(id, { ...params as any, [SkipAccessControl]: true }) as ServiceEntity<N>
     return callback(entity)
   }
