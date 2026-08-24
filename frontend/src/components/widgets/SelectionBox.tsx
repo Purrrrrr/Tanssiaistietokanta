ed923abe375aecdf48d8fc51d90816cfb9880775
Purr Consuegra <purr.consuegra@solita.fi>
2026-08-24 13:28:06 +0300

    Make SelectionBox hover color consistent with minimal Buttons

--- frontend/src/components/widgets/SelectionBox.tsx
+++ frontend/src/components/widgets/SelectionBox.tsx
@@ -3,7 +3,7 @@ import { useId } from 'react'
 export function SelectionBox(props: Pick<React.ComponentProps<'input'>, 'checked' | 'value' | 'onChange'>) {
   const id = useId()
 
-  return <label htmlFor={id} className="rounded-sm p-2 -mx-1 -my-2 hover:bg-sky-600/20 hover:*:not-checked:opacity-90 text-center">
+  return <label htmlFor={id} className="rounded-sm p-2 -mx-1 -my-2 hover:bg-stone-800/10 [:has(:checked)]:hover:bg-sky-600/20 hover:*:not-checked:opacity-90 text-center">
     <input className="accent-sky-700" id={id} type="checkbox" {...props} />
   </label>
 }
